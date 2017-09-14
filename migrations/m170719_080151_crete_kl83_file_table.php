93bad692c0b3ba743636c8354cf578f048ff2574
Aleksandr Solovyov <solovyov.a.a.19@gmail.com>
2017-09-14 16:23:28 +0300

    Fix migration

--- migrations/m170719_080151_crete_kl83_file_table.php
+++ migrations/m170719_080151_crete_kl83_file_table.php
@@ -15,8 +15,7 @@ class m170719_080151_crete_kl83_file_table extends Migration
             'fileSetId' => Schema::TYPE_INTEGER . ' unsigned NOT NULL',
             'createdAt' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP',
             'createdBy' => Schema::TYPE_INTEGER . ' unsigned NOT NULL',
-            'path' => $this->string(2500) . ' NOT NULL',
-            'url' => $this->string(2500) . ' NOT NULL',
+            'relPath' => $this->string(2500) . ' NOT NULL',
         ]);
         $this->createIndex('fileSetId', self::TABLE_NAME, 'fileSetId');
     }
