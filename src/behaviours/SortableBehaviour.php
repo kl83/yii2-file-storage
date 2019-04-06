ff2c5223d23115c18aa4d9f3fc294d02f4e48f07
Aleksandr Solovyov <solovyov.a.a.19@gmail.com>
2019-04-06 17:36:16 +0300

    When reordering files, only files of the same level are updated

--- src/behaviours/SortableBehaviour.php
+++ src/behaviours/SortableBehaviour.php
@@ -4,30 +4,23 @@ namespace kl83\filestorage\behaviours;
 
 use kl83\behaviours\SortableBehaviour as BaseBehaviour;
 use kl83\filestorage\models\File;
-use yii\db\Query;
 
 /**
  * @property File $owner
- * @property string $ownerClassName
- * @property string $primaryKey
- * @property string $sortField
  */
 class SortableBehaviour extends BaseBehaviour
 {
     protected function reorder($parentId = 0)
     {
-        $query = call_user_func("$this->ownerClassName::find");
-        /* @var $query Query */
-        $siblingFilesId = $query->select($this->primaryKey)
-            ->orderBy("$this->sortField, $this->primaryKey")
-            ->where(['fileSetId' => $this->owner->fileSetId])
-            ->column();
-        foreach ($siblingFilesId as $idx => $pk) {
-            call_user_func(
-                "$this->ownerClassName::updateAll",
-                [$this->sortField => ($idx + 1) * 100],
-                [$this->primaryKey => $pk]
-            );
+        $files = File::find()
+            ->where([
+                'fileSetId' => $this->owner->fileSetId,
+            ])
+            ->orderBy('idx, id')
+            ->all();
+        foreach ($files as $idx => $file) {
+            $file->idx = ($idx + 1) * 100;
+            $file->save(false);
         }
     }
 }
